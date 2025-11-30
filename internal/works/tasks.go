878d305338586cc19ec79e840c98790b8c8f3e5e
Madjim <maxbabintsev@gmail.com>
2025-12-01 00:04:30 +0300

    refactor: greet(), StartGreets()

--- internal/works/tasks.go
+++ internal/works/tasks.go
@@ -1,10 +1,14 @@
 package works
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
-func greet(number int, timeSleep float64) func() (int, time.Duration) {
+func greet(number int, timeSleep float64, wg *sync.WaitGroup) func() (int, time.Duration) {
+	defer wg.Done()
 	start := time.Now()
-	time.Sleep(time.Duration(timeSleep) * time.Second)
+	time.Sleep(time.Duration(timeSleep * float64(time.Second)))
 	end := time.Since(start)
 
 	return func() (int, time.Duration) {
@@ -13,7 +17,12 @@ func greet(number int, timeSleep float64) func() (int, time.Duration) {
 }
 
 func StartGreets(n int, m float64) {
+	var wg sync.WaitGroup
+	wg.Add(n)
+
 	for i := 0; i < n; i++ {
-		go greet(i, m)
+		go greet(i, m, &wg)
 	}
+
+	wg.Wait()
 }
