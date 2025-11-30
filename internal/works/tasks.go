e68319b23bea60849bb693a439d181b4776514c1
Madjim <maxbabintsev@gmail.com>
2025-11-30 23:12:04 +0300

    feat: StartGreets()     - заводит заданное число горутин
    fix: исправлена горутина greet()

--- internal/works/tasks.go
+++ internal/works/tasks.go
@@ -1,10 +1,19 @@
 package works
 
-func greet(c chan int) func() int {
-	i := 0
-	c <- i
-	return func() int {
-		i++
-		return i
+import "time"
+
+func greet(number int, timeSleep float64) func() (int, time.Duration) {
+	start := time.Now()
+	time.Sleep(time.Duration(timeSleep) * time.Second)
+	end := time.Since(start)
+
+	return func() (int, time.Duration) {
+		return number, end
+	}
+}
+
+func StartGreets(n int, m float64) {
+	for i := 0; i < n; i++ {
+		go greet(i, m)
 	}
 }
